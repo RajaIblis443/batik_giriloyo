c6d078ef0f5652c160bf72eb612d12c1becdbf67
NOOBPLAYER-DEBUG <77473102+NOOBPLAYER-DEBUG@users.noreply.github.com>
2025-07-18 09:17:37 +0700

    INI NAmabah Judul

---
+++ src/components/icons/IconGiriloyo.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="w-8 h-8 text-amber-400">
+    <svg viewBox="0 0 24 24" fill="currentColor" class="w-full h-full">
+      <path
+        d="M9 2l1 1v18l-1 1H3l-1-1V3l1-1h6zm5 1.5L21 7v10l-7 3.5V3.5zM4 4v16h4V4H4zm10 1l5 2v8l-5 2V5z"
+      />
+    </svg>
+  </div>
+</template>
